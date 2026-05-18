f12dc02a91bafdfca8f917226563ec5dc0e64360
Pierre Baillargeon <pierrebai@hotmail.com>
2026-05-17 20:18:18 -0400

    Added support for different move-sets: - kn: knight - zb: zebra - an: antelope - fe: ferz - al: alfil - wa: wazir - da: dabbaba - dr: dromedary
    The move set option must be provided after the name of the PNG file on the command-line.

--- README.md
+++ README.md
@@ -8,6 +8,19 @@ The C++ program creates a PNG of the resulting pattern. The program takes 3 opti
 - The number of knights (defaults to 2)
 - The side length of the spiral (defaults to 1000)
 - The name of the PNG file (defaults to knights.png)
+- The move set of the first piece (defaults to kn for knight)
+- The move set of the second piece (defaults to kn for knight)
+- etc
+
+The available move sets are:
+- kn: knight: jumps to 2 x 1 away
+- zb: zebra: jumps to 3 x 2 away
+- an: antelope: jumps to 4 x 3 away
+- fe: ferz: jumps to 1 x 1 away
+- al: alfil: jumps to 2 x 2 away
+- wa: wazir: jumps to 0 x 1 away
+- da: dabbaba: jumps to 0 x 2 away
+- dr: dromedary: jumps to 0 x 3 away
 
 The program is quite fast. On my lowly Intel i3770 from 2008, generating a spiral
 of side 1000 is nearly instantaneous. One of size 10 000 takes a few seconds.
@@ -15,3 +28,7 @@ of side 1000 is nearly instantaneous. One of size 10 000 takes a few seconds.
 Here is an image showing the result of 4 knights in a 10 000 x 10 000 spiral:
 
 ![4 knights 10k spiral](4-colors-10k-spiral-knights.png)
+
+Here is an image showing the result of a knight and a zebra in a 4 000 x 4 000 spiral:
+
+![knight and zebra 4k spiral](knights-and-zebra.png)
--- spiral_knights.cpp
+++ spiral_knights.cpp
@@ -18,6 +18,72 @@ const vector<pair<int64_t, int64_t>> knight_moves_deltas = {
     {1, 2}, {1, -2}, {-1, 2}, {-1, -2}
 };
 
+// A zebra moves in an L shape: 3 in one direction and 2 in the other.
+const vector<pair<int64_t, int64_t>> zebra_moves_deltas = {
+    {3, 2}, {3, -2}, {-3, 2}, {-3, -2},
+    {2, 3}, {2, -3}, {-2, 3}, {-2, -3}
+};
+
+// An antelope moves in an L shape: 4 in one direction and 3 in the other.
+const vector<pair<int64_t, int64_t>> antelope_moves_deltas = {
+    {4, 3}, {4, -3}, {-4, 3}, {-4, -3},
+    {3, 4}, {3, -4}, {-3, 4}, {-3, -4}
+};
+
+// An ferz moves in a diagonal pattern.
+const vector<pair<int64_t, int64_t>> ferz_moves_deltas = {
+    {1, 1}, {1, -1}, {-1, 1}, {-1, -1}
+};
+
+// An alfil moves in a diagonal pattern.
+const vector<pair<int64_t, int64_t>> alfil_moves_deltas = {
+    {2, 2}, {2, -2}, {-2, 2}, {-2, -2}
+};
+
+// An wazir moves in a orthogonal pattern.
+const vector<pair<int64_t, int64_t>> wazir_moves_deltas = {
+    {0, 1}, {1, 0}, {-1, 0}, {0, -1}
+};
+
+// An dabbaba moves in a orthogonal pattern.
+const vector<pair<int64_t, int64_t>> dabbaba_moves_deltas = {
+    {0, 2}, {2, 0}, {-2, 0}, {0, -2}
+};
+
+// An dromedary moves in a orthogonal pattern.
+const vector<pair<int64_t, int64_t>> dromedary_moves_deltas = {
+    {0, 3}, {3, 0}, {-3, 0}, {0, -3}
+};
+
+const vector<pair<int64_t, int64_t>> moves_for_piece_type(const char* piece_type) {
+    if (strcmp(piece_type, "kn") == 0) {
+        return knight_moves_deltas;
+    } else if (strcmp(piece_type, "zb") == 0) {
+        return zebra_moves_deltas;
+    } else if (strcmp(piece_type, "an") == 0) {
+        return antelope_moves_deltas;
+    } else if (strcmp(piece_type, "fe") == 0) {
+        return ferz_moves_deltas;
+    } else if (strcmp(piece_type, "al") == 0) {
+        return alfil_moves_deltas;
+    } else if (strcmp(piece_type, "wa") == 0) {
+        return wazir_moves_deltas;
+    } else if (strcmp(piece_type, "da") == 0) {
+        return dabbaba_moves_deltas;
+    } else if (strcmp(piece_type, "dr") == 0) {
+        return dromedary_moves_deltas;
+    }
+    throw std::invalid_argument("Unknown piece type");
+}
+
+const vector<pair<int64_t, int64_t>> moves_for_piece_type(int argc, const char** args, int index) {
+    if (argc > index) {
+        return moves_for_piece_type(args[index]);
+    } else {
+        return knight_moves_deltas;
+    }
+}
+
 // This is a list of positions that are already occupied by knights.
 // We will use this to check if a knight can be placed at a given
 // position without being attacked by another knight of a different color.
@@ -115,20 +181,11 @@ int64_t position_to_spiral_number(pair<int64_t, int64_t> position) {
 }
 
 
-vector<pair<int64_t, int64_t>> knight_attacks_for_position(pair<int64_t, int64_t> start) {
-    vector<pair<int64_t, int64_t>> attacks;
-    for (auto& delta : knight_moves_deltas) {
-        attacks.push_back({start.first + delta.first, start.second + delta.second});
-    }
-    return attacks;
-}
-
-
-void fill_attacked_positions(int64_t knight_position_number, int64_t knight_color) {
+void fill_attacked_positions(int64_t knight_position_number, int64_t knight_color, const vector<pair<int64_t, int64_t>>& deltas) {
     pair<int64_t, int64_t> knight_position = spiral_number_to_position(knight_position_number);
     const int8_t attack_bit = knight_color_to_attack_bit(knight_color);
-    
-    for (auto& delta : knight_moves_deltas) {
+
+    for (auto& delta : deltas) {
         pair<int64_t, int64_t> attacked_position = {knight_position.first + delta.first, knight_position.second + delta.second};
         int64_t attacked_position_number = position_to_spiral_number(attacked_position);
         if (attacked_position_number >= static_cast<int64_t>(occupied_positions.size())) {
@@ -156,116 +213,139 @@ bool is_position_valid_for_color(int64_t position_number, int64_t knight_color) 
 
 int main(int argc, const char** argv) {
 
-    int8_t number_of_knight_colors = int8_t(std::max(1, min(8, argc > 1 ? atoi(argv[1]) : 2)));
-    int8_t current_color = 0;
+    try {
+        int8_t number_of_knight_colors = int8_t(std::max(1, min(8, argc > 1 ? atoi(argv[1]) : 2)));
+        if (number_of_knight_colors > 8) {
+            throw std::invalid_argument("Number of knight colors must be between 1 and 8");
+        }
+        int8_t current_color = 0;
 
-    int8_t all_attacks = 0;
-    for (int64_t i = 0; i < number_of_knight_colors; i++) {
-        all_attacks |= knight_color_to_attack_bit(i);
-    }
+        int8_t all_attacks = 0;
+        for (int64_t i = 0; i < number_of_knight_colors; i++) {
+            all_attacks |= knight_color_to_attack_bit(i);
+        }
 
-    int64_t square_size = int64_t(std::max(3, argc > 2 ? atoi(argv[2]) : 1000));
-    int64_t square_area = square_size * square_size;
-    // occupied_positions.resize(size_t(square_area * 1.1), 0);
+        int64_t square_size = int64_t(std::max(3, argc > 2 ? atoi(argv[2]) : 1000));
+        int64_t square_area = square_size * square_size;
+        // occupied_positions.resize(size_t(square_area * 1.1), 0);
 
-    std::vector<int64_t> lowest_unoccupied_for_colors(number_of_knight_colors, 0);
-    // int64_t lowest_unoccupied = 0;
+        string png_filename = argc > 3 ? argv[3] : "knights.png";
 
-    while (true) {
-        int64_t possible_number = find_lowest_unoccupied_position_for_knight(lowest_unoccupied_for_colors[current_color], current_color);
-        // lowest_unoccupied = find_lowest_unoccupied_position(lowest_unoccupied, all_attacks);
-        // int64_t possible_number = lowest_unoccupied;
-        if (possible_number > square_area)
-           break;
+        vector<vector<pair<int64_t, int64_t>>> knight_moves_deltas_by_color = {
+            moves_for_piece_type(argc, argv, 4),
+            moves_for_piece_type(argc, argv, 5),
+            moves_for_piece_type(argc, argv, 6),
+            moves_for_piece_type(argc, argv, 7),
+            moves_for_piece_type(argc, argv, 8),
+            moves_for_piece_type(argc, argv, 9),
+            moves_for_piece_type(argc, argv, 10),
+            moves_for_piece_type(argc, argv, 11),
+        };
+
+        vector<int64_t> lowest_unoccupied_for_colors(number_of_knight_colors, 0);
+        // int64_t lowest_unoccupied = 0;
+
         while (true) {
-            if (possible_number >= static_cast<int64_t>(occupied_positions.size())) {
-                occupied_positions.resize(possible_number + 1, 0);
-                occupied_positions[possible_number] = -current_color - 1;
-                fill_attacked_positions(possible_number, current_color);
-                lowest_unoccupied_for_colors[current_color] = possible_number + 1;
-                break;
-            } else if (is_position_valid_for_color(possible_number, current_color)) {
-                occupied_positions[possible_number] = -current_color - 1;
-                fill_attacked_positions(possible_number, current_color);
-                lowest_unoccupied_for_colors[current_color] = possible_number + 1;
-                break;
+            int64_t possible_number = find_lowest_unoccupied_position_for_knight(lowest_unoccupied_for_colors[current_color], current_color);
+            // lowest_unoccupied = find_lowest_unoccupied_position(lowest_unoccupied, all_attacks);
+            // int64_t possible_number = lowest_unoccupied;
+            if (possible_number > square_area)
+            break;
+            while (true) {
+                if (possible_number >= static_cast<int64_t>(occupied_positions.size())) {
+                    occupied_positions.resize(possible_number + 1, 0);
+                    occupied_positions[possible_number] = -current_color - 1;
+                    fill_attacked_positions(possible_number, current_color, knight_moves_deltas_by_color[current_color]);
+                    lowest_unoccupied_for_colors[current_color] = possible_number + 1;
+                    break;
+                } else if (is_position_valid_for_color(possible_number, current_color)) {
+                    occupied_positions[possible_number] = -current_color - 1;
+                    fill_attacked_positions(possible_number, current_color, knight_moves_deltas_by_color[current_color]);
+                    lowest_unoccupied_for_colors[current_color] = possible_number + 1;
+                    break;
+                }
+                possible_number += 1;
             }
-            possible_number += 1;
+            current_color = (current_color + 1) % number_of_knight_colors;
         }
-        current_color = (current_color + 1) % number_of_knight_colors;
+
+        // Note: we add one because if the square size is even, the center of the square is between four positions,
+        // and we want to include the position at the top right of the center.
+        // vector<vector<char>> board(square_size + 1, vector<char>(square_size + 1, ' '));
+        
+        // for (int64_t i = 0; i < square_area; i++) {
+        //     pair<int64_t, int64_t> pos = spiral_number_to_position(i);
+        //     int64_t x = pos.first + square_size / 2;
+        //     int64_t y = pos.second + square_size / 2;
+        //     if (occupied_positions[i] < 0) {
+        //         board[y][x] = '0' + static_cast<char>(-occupied_positions[i]);
+        //     } else if (occupied_positions[i] > 0) {
+        //         board[y][x] = '.';
+        //     } else {
+        //         board[y][x] = ' ';
+        //     }
+        // }
+
+        // for (auto& row : board) {
+        //     for (auto& cell : row) {
+        //         cout << cell;
+        //     }
+        //     cout << '\n';
+        // }
+
+        int img_width = static_cast<int>(square_size + 3);
+        int img_height = static_cast<int>(square_size + 3);
+        int x_offset = 1;
+        int y_offset = 1;
+
+        vector<uint8_t> image(static_cast<size_t>(img_width) * static_cast<size_t>(img_height) * 4, 255);
+        const vector<array<uint8_t, 3>> palette = {
+            array<uint8_t, 3>{220,   0,   0},
+            array<uint8_t, 3>{  0, 110, 220},
+            array<uint8_t, 3>{200, 200,   0},
+            array<uint8_t, 3>{  0, 200,  80},
+            array<uint8_t, 3>{210, 140,  80},
+            array<uint8_t, 3>{  0, 220, 220},
+            array<uint8_t, 3>{128, 160, 130},
+            array<uint8_t, 3>{128,   0, 220}
+        };
+
+        for (int64_t i = 0; i < square_area; i++) {
+            pair<int64_t, int64_t> pos = spiral_number_to_position(i);
+            int64_t x = pos.first + square_size / 2 + x_offset;
+            int64_t y = pos.second + square_size / 2 + y_offset;
+            if (x < 0 || x >= img_width || y < 0 || y >= img_height) {
+                continue;
+            }
+
+            size_t pixel_index = (static_cast<size_t>(y) * static_cast<size_t>(img_width) + static_cast<size_t>(x)) * 4;
+            if (occupied_positions[i] < 0) {
+                int color_index = static_cast<int>(-occupied_positions[i] - 1);
+                const auto& color = palette[color_index % static_cast<int>(palette.size())];
+                image[pixel_index + 0] = color[0];
+                image[pixel_index + 1] = color[1];
+                image[pixel_index + 2] = color[2];
+                image[pixel_index + 3] = 255;
+            } else {
+                image[pixel_index + 0] = 0;
+                image[pixel_index + 1] = 0;
+                image[pixel_index + 2] = 0;
+                image[pixel_index + 3] = 255;
+            }
+        }
+
+        if (!stbi_write_png(png_filename.c_str(), img_width, img_height, 4, image.data(), img_width * 4)) {
+            cerr << "Failed to write PNG file: " << png_filename << '\n';
+            return 1;
+        }
+
+        cout << "Wrote PNG: " << png_filename << '\n';
+        return 0;
     }
-
-    // Note: we add one because if the square size is even, the center of the square is between four positions,
-    // and we want to include the position at the top right of the center.
-    // vector<vector<char>> board(square_size + 1, vector<char>(square_size + 1, ' '));
-    
-    // for (int64_t i = 0; i < square_area; i++) {
-    //     pair<int64_t, int64_t> pos = spiral_number_to_position(i);
-    //     int64_t x = pos.first + square_size / 2;
-    //     int64_t y = pos.second + square_size / 2;
-    //     if (occupied_positions[i] < 0) {
-    //         board[y][x] = '0' + static_cast<char>(-occupied_positions[i]);
-    //     } else if (occupied_positions[i] > 0) {
-    //         board[y][x] = '.';
-    //     } else {
-    //         board[y][x] = ' ';
-    //     }
-    // }
-
-    // for (auto& row : board) {
-    //     for (auto& cell : row) {
-    //         cout << cell;
-    //     }
-    //     cout << '\n';
-    // }
-
-    string png_filename = argc > 3 ? argv[3] : "knights.png";
-    int img_width = static_cast<int>(square_size + 3);
-    int img_height = static_cast<int>(square_size + 3);
-    int x_offset = 1;
-    int y_offset = 1;
-
-    vector<uint8_t> image(static_cast<size_t>(img_width) * static_cast<size_t>(img_height) * 4, 255);
-    const vector<array<uint8_t, 3>> palette = {
-        array<uint8_t, 3>{220,   0,   0},
-        array<uint8_t, 3>{  0, 110, 220},
-        array<uint8_t, 3>{200, 200,   0},
-        array<uint8_t, 3>{  0, 200,  80},
-        array<uint8_t, 3>{210, 140,  80},
-        array<uint8_t, 3>{  0, 220, 220},
-        array<uint8_t, 3>{128, 160, 130},
-        array<uint8_t, 3>{128,   0, 220}
-    };
-
-    for (int64_t i = 0; i < square_area; i++) {
-        pair<int64_t, int64_t> pos = spiral_number_to_position(i);
-        int64_t x = pos.first + square_size / 2 + x_offset;
-        int64_t y = pos.second + square_size / 2 + y_offset;
-        if (x < 0 || x >= img_width || y < 0 || y >= img_height) {
-            continue;
-        }
-
-        size_t pixel_index = (static_cast<size_t>(y) * static_cast<size_t>(img_width) + static_cast<size_t>(x)) * 4;
-        if (occupied_positions[i] < 0) {
-            int color_index = static_cast<int>(-occupied_positions[i] - 1);
-            const auto& color = palette[color_index % static_cast<int>(palette.size())];
-            image[pixel_index + 0] = color[0];
-            image[pixel_index + 1] = color[1];
-            image[pixel_index + 2] = color[2];
-            image[pixel_index + 3] = 255;
-        } else {
-            image[pixel_index + 0] = 0;
-            image[pixel_index + 1] = 0;
-            image[pixel_index + 2] = 0;
-            image[pixel_index + 3] = 255;
-        }
-    }
-
-    if (!stbi_write_png(png_filename.c_str(), img_width, img_height, 4, image.data(), img_width * 4)) {
-        cerr << "Failed to write PNG file: " << png_filename << '\n';
+    catch (const std::exception& e) {
+        cerr << "Error: " << e.what() << '\n';
+        cerr << "Usage: " << argv[0] << " [number_of_knight_colors] [square_size] [output_png_filename] [move_deltas_for_color_1] [move_deltas_for_color_2] ...\n";
+        cerr << "Example: " << argv[0] << " 3 1000 knights.png kn zb an\n";
         return 1;
     }
-
-    cout << "Wrote PNG: " << png_filename << '\n';
-    return 0;
 }
